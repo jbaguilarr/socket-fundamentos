2c6f219a7dc7a678b772ef26bb659ff9ddd3c2a0
bruno aguilar <josebrunoaguilar@gmail.com>
2019-04-08 21:58:03 -0400

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,23 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('conectado al servidor');
+});
+
+// escuchar
+socket.on('disconnect', function() {
+    console.log('Perdimos conexion con el servidor');
+});
+
+// Enviar informacion
+socket.emit('enviarMensaje', {
+    usuario: 'Bruno',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server:', resp);
+});
+
+// Escuchar informacion
+socket.on('enviarMensaje', function(mensaje) {
+    console.log('Servidor:', mensaje);
+});
